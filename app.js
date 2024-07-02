a00d25388c2d4462b4397a5e0c7242442a99b0f4
Abubakar Alaya <ecode5814@gmail.com>
2024-07-02 20:18:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 import express from "express";
 
-import task_1 from "./task 1/app.js";
+import task from "./task 1/app.js";
 
 const app = express();
-
 app.use('/', task_1);
 
 app.listen(process.env.PORT || 3000, () => console.log("Server running on https://localhost:3000"));
